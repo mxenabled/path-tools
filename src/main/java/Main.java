63ff448f9664bf48b698353a120c4558b360b997
Matthew Nichols <matthew.nichols@mx.com>
2022-10-04 09:59:40 -0600

    feat(main): output args
    Output 1st commandline arg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
   public static void main(String... args) {
+    if (args.length > 0) {
+      System.out.println("args:");
+      System.out.println("  " + args[0]);
+    }
+
     System.out.println("Hello world!");
     System.exit(0);
   }
