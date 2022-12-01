e97a3a5208526fe25e18737d0344bed908fdf5c3
Matt Nichols <Matthew.Nichols@mx.com>
2022-12-01 15:23:10 -0700

    feat: add arguments to bootstrap script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -12,6 +12,50 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
+# Args
+REPOSITORY="https://artifactory.internal.mx:443"
+FORCE=false
+while getopts 't:r:n:a:f' opt; do
+  case "$opt" in
+    t)
+    PROJECT_TYPE=${OPTARG}
+    ;;
+
+    r)
+    REPOSITORY=${OPTARG}
+    ;;
+
+    n)
+    NAME=${OPTARG}
+    ;;
+
+    a)
+    ACCESSOR=${OPTARG}
+    ;;
+
+    f)
+    FORCE=true
+    ;;
+
+    ?|h)
+    echo ""
+    echo "Usage: ./bootstrap [OPTIONS]"
+    echo "  -f: force delete existing folder"
+    echo "  -r [REPOSITORY]: repository (default https://artifactory.internal.mx:443)"
+    echo "  -t [PROJECT_TYPE]: project type (a - accessor, c - connector)"
+    echo "  -a [ACCESSOR]: accessor coordinates"
+    echo "  -a [NAME]: project name"
+    echo ""
+    exit 1
+    ;;
+  esac
+done
+
+
+
+
+
+
 echo "Checking prerequisites..."
 echo
 if ! command -v javac &> /dev/null
@@ -37,20 +81,27 @@ else
   echo -e "${GREEN}Gradle installed.${NC}"
 fi
 
-while true; do
-  echo "Project types:"
-  echo "  a - accessor"
-  echo "  c - connector"
-  read -p "Enter the type  " projectType
-  case $projectType in
-      [a]* ) break;;
-      [c]* ) break;;
-      * ) echo -e "${RED}Invalid type $projectType${NC}";;
-  esac
-done
+if [[ -z ${PROJECT_TYPE} ]]; then
+  while true; do
+    echo "Project types:"
+    echo "  a - accessor"
+    echo "  c - connector"
+    read -p "Enter the type  " PROJECT_TYPE
+    case $PROJECT_TYPE in
+        [a]* ) break;;
+        [c]* ) break;;
+        * ) echo -e "${RED}Invalid type $PROJECT_TYPE${NC}";;
+    esac
+  done
+fi
 
-if [ "$projectType" = "a" ]; then
-  read -p "Enter name of system to integrate (pascal-case. example: CheckFree) " systemName
+if [ "$PROJECT_TYPE" = "a" ]; then
+  if [[ -z ${NAME} ]]; then
+    read -p "Enter name of system to integrate (pascal-case. example: CheckFree) " systemName
+  else
+    systemName="$NAME"
+  fi
+
   systemNameLc="$(echo "$systemName" | tr [:upper:] [:lower:])" ## lowercase the project name
   projectFolder="path-accessor-$systemNameLc"
 
@@ -58,7 +109,12 @@ if [ "$projectType" = "a" ]; then
 
   if [ -d "$projectFolder" ]; then
     while true; do
-      read -p "$projectFolder already exists. (d)elete, (c)ontinue, (a)bort ? " dca
+      if [ $FORCE ]; then
+        dca="d"
+      else
+        read -p "$projectFolder already exists. (d)elete, (c)ontinue, (a)bort ?  " dca
+      fi
+
       case $dca in
         [Dd]* ) rm -Rf "$projectFolder"; break;;
         [Cc]* ) break;;
@@ -79,12 +135,67 @@ if [ "$projectType" = "a" ]; then
   gradle wrapper --distribution-type all
 
   echo -e "${GREEN}Scaffolding accessor project...${NC}"
-  ./gradlew initAccessor --name=$systemName --language=JAVA --javaVersion=VERSION_17
+  ./gradlew initAccessorProject --name=$systemName --language=JAVA --javaVersion=VERSION_17
   ./gradlew assemble
 
   echo ""
   echo -e "${GREEN}-----------------------------------------------${NC}"
-  echo -e "${GREEN}- Project setup in $projectFolder${NC}"
+  echo -e "${GREEN}- Accessor setup in $projectFolder${NC}"
+  echo -e "${GREEN}-----------------------------------------------${NC}"
+  echo ""
+  exit 0
+fi
+
+if [ "$PROJECT_TYPE" = "c" ]; then
+  if [[ -z ${NAME} ]]; then
+    read -p "Enter name client (pascal-case. example: AmericaFirst)  " clientName
+  else
+    clientName="$NAME"
+  fi
+
+  clientNameLc="$(echo "$clientName" | tr [:upper:] [:lower:])" ## lowercase the project name
+  projectFolder="path-connector-$clientNameLc"
+
+  if [[ -z ${ACCESSOR} ]]; then
+    read -p "Accessor coordinates (example: com.mx.path.service.accessor:qolo)  " ACCESSOR
+  fi
+
+  echo -e "${GREEN}Setting up connector project in folder $projectFolder...${NC}"
+
+  if [ -d "$projectFolder" ]; then
+    while true; do
+      if [ $FORCE ]; then
+        dca="d"
+      else
+        read -p "$projectFolder already exists. (d)elete, (c)ontinue, (a)bort ?  " dca
+      fi
+
+      case $dca in
+        [Dd]* ) rm -Rf "$projectFolder"; break;;
+        [Cc]* ) break;;
+        * ) echo "Exiting..."; exit 1;;
+      esac
+    done
+  fi
+
+  echo -e "${GREEN}Creating project folder $projectFolder...${NC}"
+  mkdir "./$projectFolder"
+  cd "$projectFolder"
+
+  echo -e "${GREEN}Loading bootstrap gradle files...${NC}"
+  curl -O https://raw.githubusercontent.com/mxenabled/path-tools/master/bootstrap/template/build.gradle
+  curl -O https://raw.githubusercontent.com/mxenabled/path-tools/master/bootstrap/template/settings.gradle
+
+  echo -e "${GREEN}Setting up gradle wrapper...${NC}"
+  gradle wrapper --distribution-type all
+
+  echo -e "${GREEN}Scaffolding accessor project...${NC}"
+  ./gradlew --stacktrace initConnectorProject --name=$clientName --clientId=$clientNameLc --language=JAVA --javaVersion=VERSION_17 --accessor=$ACCESSOR --repository=$REPOSITORY
+  ./gradlew assemble
+
+  echo ""
+  echo -e "${GREEN}-----------------------------------------------${NC}"
+  echo -e "${GREEN}- Connector setup in $projectFolder${NC}"
   echo -e "${GREEN}-----------------------------------------------${NC}"
   echo ""
   exit 0
